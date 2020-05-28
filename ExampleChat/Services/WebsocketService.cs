00f9e9450ebf968bdec3e35f206ddb5f6ce95e9b
renanrcp <renancamerapinheiro@gmail.com>
2020-05-27 22:36:29 -0300

    added events

--- ExampleChat/Services/WebsocketService.cs
+++ ExampleChat/Services/WebsocketService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using MariSocketMiddleware.Entities.MariEventArgs;
 using MariSocketMiddleware.Services;
 using Microsoft.AspNetCore.Http;
 
@@ -8,11 +10,19 @@ namespace ExampleChat.Middlewares
     {
         public WebsocketService()
         {
+            OnOpen += OnOpenAsync;
+            OnMessage += OnMessageAsync;
         }
 
         public override string Path { get; set; }
 
         public override Task<bool> AuthorizeAsync(HttpContext context)
             => Task.FromResult(true);
+
+        private Task OnOpenAsync(OpenEventArgs arg)
+            => Task.CompletedTask;
+
+        private Task OnMessageAsync(MessageEventArgs arg)
+            => Task.CompletedTask;
     }
 }
